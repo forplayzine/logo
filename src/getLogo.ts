6ce33ad6d771cd19e95012f2e406b9a6e7eceb11
wandyezj <wandyezj@gmail.com>
2023-03-08 19:05:50 -0800

    letter P

--- src/getLogo.ts
+++ src/getLogo.ts
@@ -1,6 +1,7 @@
 import { getSvg } from "./getSvg";
 import { getSvgRect } from "./getSvgRect";
-import { getSvgCircle } from "./getSvgCircle";
+//import { getSvgCircle } from "./getSvgCircle";
+import { getLetterP } from "./getLetterP";
 /**
  * Example Documentation
  * @returns "hello"
@@ -29,44 +30,55 @@ export function getLogo(): string {
     // P
     //
 
-    // | down
-    elements.push(getSvgRect({ x: pStartX, y: pStartY, width: lineWidth, height: pHeight }));
+    // // | down
+    // elements.push(getSvgRect({ x: pStartX, y: pStartY, width: lineWidth, height: pHeight }));
 
-    // = across
-    const pAcrossHeight = 20;
-    const pAcrossWidth = 40;
-    elements.push(
-        getSvgRect({ x: pStartX, y: pStartY, width: pAcrossWidth, height: pAcrossHeight })
-    );
+    // // = across
+    // const pAcrossHeight = 20;
+    // const pAcrossWidth = 40;
+    // elements.push(
+    //     getSvgRect({ x: pStartX, y: pStartY, width: pAcrossWidth, height: pAcrossHeight })
+    // );
 
-    // O at end
-    // center of circle is
-    const pCircleCenterX = pStartX + pAcrossWidth;
-    const pCircleCenterY = pStartY + pAcrossHeight / 2;
-    const pCircleRadius = pAcrossHeight / 2;
-    elements.push(getSvgCircle({ x: pCircleCenterX, y: pCircleCenterY, radius: pCircleRadius }));
+    // // O at end
+    // // center of circle is
+    // const pCircleCenterX = pStartX + pAcrossWidth;
+    // const pCircleCenterY = pStartY + pAcrossHeight / 2;
+    // const pCircleRadius = pAcrossHeight / 2;
+    // elements.push(getSvgCircle({ x: pCircleCenterX, y: pCircleCenterY, radius: pCircleRadius }));
 
-    // O cutout
-    const pCircleCutoutRadius = pAcrossHeight / 3;
-    elements.push(
-        getSvgCircle({
-            x: pCircleCenterX,
-            y: pCircleCenterY,
-            radius: pCircleCutoutRadius,
-            fill: "white",
-        })
-    );
+    // // O cutout
+    // const pCircleCutoutRadius = pAcrossHeight / 3;
+    // elements.push(
+    //     getSvgCircle({
+    //         x: pCircleCenterX,
+    //         y: pCircleCenterY,
+    //         radius: pCircleCutoutRadius,
+    //         fill: "white",
+    //     })
+    // );
 
-    // = cutout
-    elements.push(
-        getSvgRect({
-            x: pStartX + lineWidth,
-            y: pStartY + pCircleRadius - pCircleCutoutRadius,
-            width: pAcrossWidth - lineWidth,
-            height: pCircleCutoutRadius * 2,
-            fill: "white",
-        })
-    );
+    // // = cutout
+    // elements.push(
+    //     getSvgRect({
+    //         x: pStartX + lineWidth,
+    //         y: pStartY + pCircleRadius - pCircleCutoutRadius,
+    //         width: pAcrossWidth - lineWidth,
+    //         height: pCircleCutoutRadius * 2,
+    //         fill: "white",
+    //     })
+    // );
+
+    const letterP = getLetterP({
+        x: pStartX,
+        y: pStartY,
+        height: pHeight,
+        width: 50,
+        armHeight: 30,
+        stemWidth: 20,
+        borderSize: 2,
+    });
+    elements.push(letterP);
 
     //
     // F
